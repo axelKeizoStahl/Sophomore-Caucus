098a1efb00ce05aeeccbf272a6a78ceb324027e6
axelKeizoStahl <axelkeizo@gmail.com>
2023-09-07 22:27:43 -0400

    added department divs/styles in about us

--- src/app/news/events/page.js
+++ src/app/news/events/page.js
@@ -1,10 +1,46 @@
 export default function Page() {
+  events = [
+    {
+      title: "Event 1",
+      date: "2021-01-01",
+      time: "12:00",
+      location: "Location 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Event 2",
+      date: "2021-01-01",
+      time: "12:00",
+      location: "Location 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Event 3",
+      date: "2021-01-01",
+      time: "12:00",
+      location: "Location 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Event 4",
+      date: "2021-01-01",
+      time: "12:00",
+      location: "Location 4",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ]
   return (
     <div className="upcoming-events">
       <div className="header">
         <h1>Upcoming Events</h1>
-        <p></p>
       </div>
+    <div className="events">
+      {events.map((event, index) => (
+        <div className={`event event${index}`} key={index}>
+        </div>
+        )
+      )}
+    </div>
     </div>
   )
 }
