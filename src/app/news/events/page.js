8847dcfc502e2ae764924ed2b4621e627c3f7d23
axelKeizoStahl <axelkeizo@gmail.com>
2023-10-02 22:43:51 -0400

    starting event grids

--- src/app/news/events/page.js
+++ src/app/news/events/page.js
@@ -4,28 +4,28 @@ import Link from 'next/link'
 export default function Page() {
   var events = [
     {
-      title: "Event 1",
+      title: "event1",
       date: "2021-01-01",
       time: "12:00",
       location: "Location 1",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      title: "Event 2",
+      title: "event2",
       date: "2021-01-01",
       time: "12:00",
       location: "Location 2",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      title: "Event 3",
+      title: "event3",
       date: "2021-01-01",
       time: "12:00",
       location: "Location 3",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      title: "Event 4",
+      title: "event4",
       date: "2021-01-01",
       time: "12:00",
       location: "Location 4",
@@ -37,12 +37,12 @@ export default function Page() {
         <h1>Upcoming Events</h1>
         <Link className={styles.past_events_link} href="/news/events/past-events"><p>Past Events</p></Link>
         <div className={styles.events}>
-          {events.map((event, index) => (
+          {events.slice(0).reverse().map((event, index) => (
             <div className={`event event${index}`} key={index}>
-                <h2>{event.title}</h2>
-                <h3>{event.date} at {event.time}</h3>
-                <h3>{event.location}</h3>
-                <p>{event.description}</p>
+              <div className={styles.event_icon}>
+                <h3>Event</h3>
+                <p>{event.date}</p>
+              </div>
             </div>
             )
           )}
