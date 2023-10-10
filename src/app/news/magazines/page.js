085503661f299331825fcbf31f57b1f90a256a99
axelKeizoStahl <axelkeizo@gmail.com>
2023-10-09 23:10:59 -0400

    magazine page

--- src/app/news/magazines/page.js
+++ src/app/news/magazines/page.js
@@ -2,10 +2,14 @@ import styles from './styles.css'
 export default function Page() {
   return (
     <div>
+      <div className="header">
         <h1>Magazines</h1>
+      </div>
       <div className="mags">
           {[1,2,3,4].map((i) => (
+              <div className='mag-container'>
               <iframe className="mag" src={`/mag${i}.pdf`} height="600" width="600" />
+              </div>
           ))}
       </div>
     </div>
