54a30e76e098ebd49508d3b6c2f2ff0e281bb2c5
axelKeizoStahl <axelkeizo@gmail.com>
2023-12-27 17:57:38 -0500

    new mags directory

--- src/app/news/magazines/page.js
+++ src/app/news/magazines/page.js
@@ -12,7 +12,7 @@ export default function Page() {
       </div>
       <div className={styles.mags}>
           {[1,2,3,4].map((i) => (
-            <iframe className={styles.mag} src={`/mag${i}.pdf#toolbar=0`} height="600" width="600" frameborder="0" />
+            <iframe className={styles.mag} src={`/mags/mag${i}.pdf#toolbar=0`} height="600" width="600" frameborder="0" />
           ))}
       </div>
       <h1 className={`${styles.end_message} `}>These are all the events we have planned for now. Check back later for more!</h1>
