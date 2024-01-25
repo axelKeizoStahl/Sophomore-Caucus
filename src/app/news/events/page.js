fbd9ae552e3e17ba65fbb7a0cba00ce2e64a254b
axelKeizoStahl <axelkeizo@gmail.com>
2024-01-25 16:46:55 -0500

    events

--- src/app/news/events/page.js
+++ src/app/news/events/page.js
@@ -7,48 +7,33 @@ import Image from 'next/image'
 import Logo from '../../logo.png'
 
 export default function Page() {
-  const [event_info_show, setEvent_info_show] = useState("1")
+  const [event_info_show, setEvent_info_show] = useState("0")
 
   var events = [
-    {
+   {
       title: "Dance",
       date: "2021-01-01",
       time: "12:00",
       location: "Location 1",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
-    {
-      title: "Picnic",
-      date: "2021-01-01",
-      time: "12:00",
-      location: "Location 2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Pizza Party",
-      date: "2021-01-01",
-      time: "12:00",
-      location: "Location 3",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Movie Night",
-      date: "2021-01-01",
-      time: "12:00",
-      location: "Location 4",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
   ]
   return (
     <div className={`${typeof event_info_show!="string" ? styles.darken:""} ${styles.upcoming_events}`}>
-        <div className={styles.navbar}></div>
-        <div className={`${styles.event_info} ${typeof event_info_show=="string" ? styles.hide : ""}`}>
-          <Image src={CloseIcon} alt="Close Icon" width="50" height="50" onClick={()=>{setEvent_info_show("1")}}/>
-          <h1>{events[event_info_show].title}</h1>
-          <p>{events[event_info_show].description}</p>
-          <p>{events[event_info_show].date}</p>
-          <p>{events[event_info_show].location}</p>
-          <p>{events[event_info_show].time}</p>
+      <div className={styles.navbar}></div>
+      <div className={`${styles.event_info} ${typeof event_info_show=="string" ? styles.hide : ""}`}>
+        <div className={styles.container}>
+          <Image src={Logo} className={styles.close_button} alt="Close Icon" width="100" height="100" onClick={()=>{setEvent_info_show("0")}}/>
+          <div className={styles.info}>
+            <h1>{events.length > 0 ? events[event_info_show].title : ""}</h1>
+            <p>{events.length > 0 ? events[event_info_show].description : ""}</p>
+            <h2> When? </h2>
+            <p>{events.length > 0 ? events[event_info_show].date : ""}</p>
+            <p>{events.length > 0 ? events[event_info_show].time : ""}</p>
+            <h2> Where? </h2>
+            <p>{events.length > 0 ? events[event_info_show].location : ""}</p>
+          </div>
+          </div>
         </div>
       <div className={styles.header}>
         <Image src={Logo} alt="Logo" width="200" height="200"/>
